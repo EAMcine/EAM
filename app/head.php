8662cd143fbee43217b8c9089a2e9e7005d83f3a
Twiloo <3rtuanu@gmail.com>
2023-03-10 10:12:44 +0100

    Ajout des favicons

--- app/head.php
+++ app/head.php
@@ -7,6 +7,9 @@ echo '
 <meta name="'.NAME_VIEWPORT.'" content="'.CONTENT_VIEWPORT.'">
 <title>'.$title.SEPARATOR.SITE_NAME.'</title>
 <link rel="stylesheet" href="/style.css">
-<link rel="icon" href="/favicon.png">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
 </head>';
