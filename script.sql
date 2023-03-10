ca6507c6a8f167d64556b1c4a74b59fff4cc23fa
wysixontheflux <bitpoch@icloud.com>
2023-03-10 14:08:21 +0100

    Add films ( sql, method )

--- script.sql
+++ script.sql
@@ -19,5 +19,21 @@ CREATE TABLE `users` (
 
 ALTER TABLE `users` CHANGE `group` `group` INT(11) NULL DEFAULT NULL;
 
+CREATE TABLE `films`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `description` text NOT NULL,
+    `year` int(11) NOT NULL,
+    `duration` int(11) NOT NULL,
+    `director` varchar(255) NOT NULL,
+    `trailer` varchar(255) NOT NULL,
+    `genre` varchar(255) NOT NULL,
+    `photo` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+)
+
 -- Path: EAM/script.sql
 
