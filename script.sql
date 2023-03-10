3ac212035b990f7c24ef9a1deb67b589a72e6768
pole-innovation-coris <122869061+pole-innovation-coris@users.noreply.github.com>
2023-03-10 11:10:38 +0100

    Script sql

--- script.sql
+++ script.sql
@@ -1,1 +1,19 @@
-test
+CREATE DATABASE EAM;
+USE EAM;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `group` int(11) NOT NULL,
+  `photo_profile` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+
+-- Path: EAM/seed.sql
+
