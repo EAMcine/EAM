531a4b432e9803c480b8b9a6756c6332c8ed6b0a
Twiloo <3rtuanu@gmail.com>
2023-03-25 16:12:57 +0100

    Basic header styling

--- views/components/header.php
+++ views/components/header.php
@@ -1,22 +1,18 @@
 
 <header role="header">
-    <div class="logo">
-        <a href="<?php echo HOME_URL; ?>"><img src="/img/logo.png" width="100px" alt="Logo EAM+"></a>
-    </div>
-    <nav class="header-menu">
+    <img class="header-logo" onclick="location.href = '<?php echo HOME_URL; ?>'" src="/img/logo.png" alt="Logo EAM+">
+    <div class="header-separator"></div>
+    <nav class="header-main">
         <?php
             foreach ($data['linkedPages'] as $pageName => $pageLink) {
-                echo "<a class='header-link' href='".HOME_URL."$pageLink'>$pageName</a>";
+                echo "<a class=\"header-link\" href='".HOME_URL."$pageLink'>$pageName</a>";
             }
         ?>
 
     </nav>
-    <!-- <nav class="menu" role="navigation">
-        <div class="m-left">
-            <a href="#">1</a>
-            <a href="#">2</a>
-
-        </div>
-
-    </nav> -->
+    <div class="header-separator"></div>
+    <nav class="header-menu">
+        <a href="#">Theme</a>
+        <a href="#">Compte</a>
+    </nav>
 </header>
