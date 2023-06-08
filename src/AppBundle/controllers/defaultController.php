59575750eca9fccaa24ceb33c05278d9b1496606
Twiloo <3rtuanu@gmail.com>
2023-06-08 11:11:55 +0200

    Fix bddInit

--- src/AppBundle/controllers/defaultController.php
+++ src/AppBundle/controllers/defaultController.php
@@ -3,12 +3,12 @@
 namespace AppBundle\Controllers;
 
 use Framework\Components\Controller as Controller;
+use \AppBundle\Traits\ControllerTrait as ControllerTrait;
 
 class DefaultController extends Controller {
-    use \AppBundle\Traits\ControllerTrait;
+    use ControllerTrait;
 
     public function __construct() {
-        parent::__construct();
         $this->sessionInfo();
     }
 }
